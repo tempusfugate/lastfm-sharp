48860421d8f05f6d8ae4c94fee9a4dc9bdbf4c1a
dkackman_2000@yahoo.com <dkackman_2000@yahoo.com@7e39177c-a1cf-11dd-91bf-632da8b6e10b>
2009-05-02 00:18:38 +0000

    Added assembly info

---
+++ branches/dev-mobile/lastfm-sharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyTitle("Last.fm.Mobile.Properties")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Last.fm.Mobile.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("65387c1c-8b8e-491a-bd75-ab49b1a7750d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+
